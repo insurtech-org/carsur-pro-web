8023ada9072af3b966486ec35a0ad83f376531d2
gs2o-insurtech <gs2o@insurtech.co.kr>
2025-11-17 15:33:56 +0900

    feat : api 스킵로딩 추가

--- src/api/comments.api.ts
+++ src/api/comments.api.ts
@@ -23,19 +23,26 @@ export interface ICommentRead {
 }
 
 //댓글 조회
-export const getComments = async (id: number) => {
-  const res = await apiInstance.get(`${prefix}/${id}/comments`);
+export const getComments = async (id: number, unreadOnly: boolean = false, skipLoading: boolean = true) => {
+  const res = await apiInstance.get(`${prefix}/${id}/comments`, {
+    params: { unreadOnly },
+    skipLoading, // skipLoading 옵션이 true인 경우 로딩을 건너뜀
+  });
   return res?.data?.data;
 };
 
 //댓글 작성
-export const postComment = async (id: number, body: ICommentCreate) => {
-  const res = await apiInstance.post(`${prefix}/${id}/comments`, body);
+export const postComment = async (id: number, body: ICommentCreate, skipLoading: boolean = true) => {
+  const res = await apiInstance.post(`${prefix}/${id}/comments`, body, {
+    skipLoading, // skipLoading 옵션이 true인 경우 로딩을 건너뜀 (기본값: true)
+  });
   return res;
 };
 
 //댓글 읽음 처리
-export const readComment = async (id: number, body: ICommentRead) => {
-  const res = await apiInstance.post(`${prefix}/${id}/comments/read`, body);
+export const readComment = async (id: number, body: ICommentRead, skipLoading: boolean = true) => {
+  const res = await apiInstance.post(`${prefix}/${id}/comments/read`, body, {
+    skipLoading, // skipLoading 옵션이 true인 경우 로딩을 건너뜀 (기본값: true)
+  });
   return res;
 };
